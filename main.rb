d34042ae89594cdbf9eaf9796ae27139026aae6a
edieatha <edieatha@gmail.com>
2020-05-27 01:17:24 +0300

    Fixed rubocop errors

--- main.rb
+++ main.rb
@@ -1,63 +1,64 @@
 module Enumerable
-  
   def my_each
-		return to_enum unless block_given?
-		n = 0
+    return to_enum unless block_given?
+
+    n = 0
     while n < self.size
-        yield(self[n])
+      yield(self[n])
       n += 1
     end
   end
 
   def my_each_with_index
-		return to_enum unless block_given?
-		n = 0
+    return to_enum unless block_given?
+
+    n = 0
     while n < self.size
-        yield(self[n], index)
+      yield(self[n], index)
       n += 1
     end
   end
 
-	def my_select(&block)
-	  result =[]
-	    self.my_each do |item|
-	      result << item if block.call(item) == true
-	    end
-	  result
-	end
+  def my_select(&block)
+    result = []
+      self.my_each do |item|
+        result << item if block.call(item) == true
+      end
+    result
+  end
 
-	  def my_all?
-		 return to_enum unless block_given?
-		   self.my_each do |i|
-		   	 return false if yield(i) == false
-		   end
-		 true
-	  end
+  def my_all?
+    return to_enum unless block_given?
 
-	  def my_any?
-		 return to_enum unless block_given?
-		   self.my_each do |i|
-		   	 return true if yield i
-		   end
-		  false
-	  end
+      self.my_each do |i|
+        return false if yield(i) == false
+      end
+    true
+  end
 
-	  def my_none?
-		return to_enum unless block_given?
-		  self.my_each do |i|
-			return false if yield(i) == true
-		   end
-		true
-	  end
+  def my_any?
+    return to_enum unless block_given?
 
-	  def my_map(&block)
-	    result = []
-		    self.my_each do |item|
-		      result << block.call(item)
-		    end
-	    result
-	  end
+      self.my_each do |i|
+        return true if yield i
+      end
+    false
+  end
 
+  def my_none?
+    return to_enum unless block_given?
 
+      self.my_each do |i|
+        return false if yield(i) == true
+      end
+    true
+  end
 
+  def my_map(&block)
+    result = []
+      self.my_each do |item|
+        result << block.call(item)
+      end
+    result
+  end
 end
