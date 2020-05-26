7be25605a4e915ce87478bcaf614c1c42d5044b6
edieatha <edieatha@gmail.com>
2020-05-27 00:31:50 +0300

    Create my_all and my_any methods

--- main.rb
+++ main.rb
@@ -17,12 +17,29 @@ module Enumerable
     end
   end
 
-def my_select(&block)
-    result =[]
-    self.my_each do |item|
-      result << item if block.call(item) == true
-    end
-    result
-  end
+	def my_select(&block)
+	    result =[]
+	    self.my_each do |item|
+	      result << item if block.call(item) == true
+	    end
+	    result
+	  end
+
+	  def my_all?
+	   return to_enum unless block_given?
+	   self.my_each do |i|
+	   	return false if yield(i) == false
+	   end
+	   true
+	  end
+
+	  def my_any?
+	   return to_enum unless block_given?
+	   self.my_each do |i|
+	   	return true if yield i
+	   end
+	   false
+	  end
+
 
 end
