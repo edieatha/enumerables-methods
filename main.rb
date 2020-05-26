50b355dbd32fd9e1b92a81668bc25e8c885486a4
edieatha <edieatha@gmail.com>
2020-05-27 00:29:11 +0300

    Create my_each_with_index enumerator

--- main.rb
+++ main.rb
@@ -7,4 +7,14 @@ module Enumerable
       n += 1
     end
   end
+
+  def my_each_with_index
+		return to_enum unless block_given?
+		n = 0
+    while n < self.size
+        yield(self[n], index)
+      n += 1
+    end
+  end
+
 end
