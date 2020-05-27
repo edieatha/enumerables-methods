509c4a591a741a828f0671a62655bb39880427de
edieatha <edieatha@gmail.com>
2020-05-27 22:15:11 +0300

    Added boolean checks for my_any? and my_all?.

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 module Enumerable
+  
+  # friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+  # friends.my_each { |friend| puts "Hello, " + friend }
+  # my_each
   def my_each
     return to_enum unless block_given?
 
@@ -9,6 +13,9 @@ module Enumerable
     end
   end
 
+  # fruits = ["apple", "banana", "strawberry", "pineapple"]
+  # fruits.each_with_index { |fruit, index| puts fruit if index.even?
+  # my_each_with_index
   def my_each_with_index
     return to_enum unless block_given?
 
@@ -19,6 +26,8 @@ module Enumerable
     end
   end
 
+  # p [1, 2, 3, 4, 6].my_select{ |i| i.even?}
+  # my_select
   def my_select(&block)
     result = []
       self.my_each do |item|
@@ -27,6 +36,9 @@ module Enumerable
     result
   end
 
+  # puts [1, 2, 3, 4].my_all?{ |i| i.even?}
+  # puts [1, 2, 3, 4].my_all?{ |i| i < 6}
+  # my_all
   def my_all?(*arg)
     case result = true
     when !arg[0].nil?
@@ -39,6 +51,10 @@ module Enumerable
     result
   end
 
+
+  # puts [1, 2, 3, 4].my_any?{ |i| i.even?}
+  # puts [1, 2, 3, 4].my_any?{ |i| i > 6}
+  # my_any
   def my_any?(*arg)
     case result = false
     when !arg[0].nil?
@@ -51,6 +67,9 @@ module Enumerable
     result
   end
 
+  # puts [1, 2, 3, 4].my_none?{ |i| i.even?}
+  # puts [1, 2, 3, 4].my_none?{ |i| i > 6}
+  # my_none
   def my_none?
     return to_enum unless block_given?
 
@@ -62,17 +81,17 @@ module Enumerable
 
   # [4, 5, 6].my_count => 3
   # [4, 5, 6].my_count(5) => 5
-  # [4, 5, 6].my_count { |x| x > 4 } => 2
+  # [4, 5, 6].my_count { |i| i > 4 } => 2
   # my_count
   def my_count(result = nil)
     return result if result
     return length unless block_given?
 
-    my_select { |x| yield x }.length
+    my_select { |i| yield i }.length
   end
 
-  # [1,2,3,4,5].my_map { |x| x * 2 }
-  # my_proc = proc{ |x| x*2 }
+  # [1,2,3,4,5].my_map { |i| i * 2 }
+  # my_proc = proc{ |i| i*2 }
   # p [1, 2, 3, 4].my_map(&my_proc)
   # my_map
   def my_map(proc=nil)
